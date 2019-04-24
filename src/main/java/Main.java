20bc4f75d42a9e3842170e918f3558a805c35dcb
Iamttp <1877784332@qq.com>
2019-04-24 17:04:17 +0800

    0.1 基本结构

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import static core.math.MathUtil.combinationCount;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(combinationCount(10, 5));
+    }
+}
---
+++ src/main/java/core/math/NumberUtil.java
@@ -0,0 +1,37 @@
+package core.math;
+
+public class NumberUtil {
+    /**
+     * 计算阶乘
+     * <p>
+     * n! = n * (n-1) * ... * end
+     * </p>
+     *
+     * @param start 阶乘起始
+     * @param end   阶乘结束
+     * @return 结果
+     * @since 4.1.0
+     */
+    public static long factorial(long start, long end) {
+        if (start < end) {
+            return 0L;
+        }
+        if (start == end) {
+            return 1L;
+        }
+        return start * factorial(start - 1, end);
+    }
+
+    /**
+     * 计算阶乘
+     * <p>
+     * n! = n * (n-1) * ... * 2 * 1
+     * </p>
+     *
+     * @param n 阶乘起始
+     * @return 结果
+     */
+    public static long factorial(long n) {
+        return factorial(n, 1);
+    }
+}
